c3b1556816d889e81d95e5a21df5a4e3f907da57
Koitharu <nvasya95@gmail.com>
2023-02-06 17:50:44 +0200

    Add headers to CloudFlareException

--- src/main/kotlin/org/koitharu/kotatsu/parsers/exception/CloudFlareProtectedException.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/exception/CloudFlareProtectedException.kt
@@ -1,7 +1,9 @@
 package org.koitharu.kotatsu.parsers.exception
 
+import okhttp3.Headers
 import okio.IOException
 
 class CloudFlareProtectedException(
 	val url: String,
+	val headers: Headers,
 ) : IOException("Protected by CloudFlare: $url")
