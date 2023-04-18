3112b7937cc0a2dd71ec6a7d7135622cb135ad17
vianh <polaris140698@gmail>
2023-04-18 23:49:46 +0700

    [DoujinDesu] Apply suggesions from code review

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/DoujinDesuParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/DoujinDesuParser.kt
@@ -1,6 +1,5 @@
 package org.koitharu.kotatsu.parsers.site
 
-import org.koitharu.kotatsu.parsers.InternalParsersApi
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.PagedMangaParser
@@ -12,7 +11,7 @@ import java.util.*
 
 @MangaSourceParser("DOUJINDESU", "DoujinDesu", "id")
 class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, MangaSource.DOUJINDESU, pageSize = 18) {
-	@InternalParsersApi
+
 	override val configKeyDomain: ConfigKey.Domain
 		get() = ConfigKey.Domain("212.32.226.234", null)
 
@@ -21,6 +20,7 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 
 	override suspend fun getDetails(manga: Manga): Manga {
 		val docs = webClient.httpGet(manga.url.toAbsoluteUrl(domain)).parseHtml().selectFirstOrThrow("#archive")
+		val chapterDateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", sourceLocale)
 		val metadataEl = docs.selectFirst(".wrapper > .metadata tbody")
 		val state = when (metadataEl?.selectFirst("tr:contains(Status)")?.selectLast("td")?.text()) {
 			"Finished" -> MangaState.FINISHED
@@ -39,20 +39,22 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 					source = source,
 				)
 			},
-			chapters = docs.select("#chapter_list > ul > li").mapChapters(reversed = true) { index, element ->
-				val titleTag = element.selectFirstOrThrow(".epsleft > .lchx > a")
-				val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale("in"))
-				MangaChapter(
-					id = generateUid(titleTag.attrAsRelativeUrl("href")),
-					name = titleTag.text(),
-					number = index + 1,
-					url = titleTag.attrAsRelativeUrl("href"),
-					scanlator = null,
-					uploadDate = dateFormat.tryParse(element.select(".epsleft > .date").text()),
-					branch = null,
-					source = source,
-				)
-			},
+			chapters = docs.requireElementById("chapter_list")
+				.select("ul > li")
+				.mapChapters(reversed = true) { index, element ->
+					val titleTag = element.selectFirstOrThrow(".epsleft > .lchx > a")
+					val url = titleTag.attrAsRelativeUrl("href")
+					MangaChapter(
+						id = generateUid(url),
+						name = titleTag.text(),
+						number = index + 1,
+						url = url,
+						scanlator = null,
+						uploadDate = chapterDateFormat.tryParse(element.select(".epsleft > .date").text()),
+						branch = null,
+						source = source,
+					)
+				},
 		)
 	}
 
@@ -60,7 +62,7 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 		page: Int,
 		query: String?,
 		tags: Set<MangaTag>?,
-		sortOrder: SortOrder
+		sortOrder: SortOrder,
 	): List<Manga> {
 		val url = urlBuilder().apply {
 			addPathSegment("manga")
@@ -80,7 +82,10 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 			}
 		}.build()
 
-		return webClient.httpGet(url).parseHtml().select("#archives > div.entries > .entry")
+		return webClient.httpGet(url).parseHtml()
+			.requireElementById("archives")
+			.selectFirstOrThrow("div.entries")
+			.select(".entry")
 			.map {
 				val titleTag = it.selectFirstOrThrow(".metadata > a")
 				val relativeUrl = titleTag.attrAsRelativeUrl("href")
@@ -105,7 +110,7 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 
 	override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
 		val id = webClient.httpGet(chapter.url.toAbsoluteUrl(domain)).parseHtml()
-			.selectFirstOrThrow("#reader")
+			.requireElementById("reader")
 			.attr("data-id")
 		return webClient.httpPost("/themes/ajax/ch.php".toAbsoluteUrl(domain), "id=$id").parseHtml()
 			.select("img")
@@ -122,7 +127,9 @@ class DoujinDesuParser(context: MangaLoaderContext) : PagedMangaParser(context, 
 
 	override suspend fun getTags(): Set<MangaTag> {
 		return webClient.httpGet("/genre/".toAbsoluteUrl(domain)).parseHtml()
-			.select("#taxonomy .entries > .entry > a")
+			.requireElementById("taxonomy")
+			.selectFirstOrThrow(".entries")
+			.select(".entry > a")
 			.mapToSet {
 				MangaTag(
 					key = it.attr("title"),
