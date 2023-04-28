b2675c3caca175b754698a1bbc30743a1af3568a
Koitharu <nvasya95@gmail.com>
2023-04-28 16:17:47 +0300

    Fix @MangaSources annotation

--- src/test/kotlin/org/koitharu/kotatsu/parsers/MangaSources.kt
+++ src/test/kotlin/org/koitharu/kotatsu/parsers/MangaSources.kt
@@ -3,5 +3,5 @@ package org.koitharu.kotatsu.parsers
 import org.junit.jupiter.params.provider.EnumSource
 import org.koitharu.kotatsu.parsers.model.MangaSource
 
-@EnumSource(MangaSource::class, names = ["HONEYMANGA"], mode = EnumSource.Mode.INCLUDE)
+@EnumSource(MangaSource::class, names = ["LOCAL", "DUMMY"], mode = EnumSource.Mode.EXCLUDE)
 internal annotation class MangaSources
