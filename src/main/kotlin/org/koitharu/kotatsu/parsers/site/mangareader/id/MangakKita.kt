7b28072ee5dfd639981f2de34f7a2ecf294ee824
devi <github.uqmqu@aleeas.com>
2023-06-28 19:59:11 +0200

    delete table mode for automatic detect mode and chage defaut value for date and list url

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/MangakKita.kt
@@ -0,0 +1,15 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.id
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.config.ConfigKey
+import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("MANGAKITA", "MangaKita", "id")
+internal class MangakKita(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.MANGAKITA, pageSize = 20, searchPageSize = 20) {
+	override val configKeyDomain: ConfigKey.Domain
+		get() = ConfigKey.Domain("mangakita.net")
+
+}
