03b4fc9f00534152e8a1946a670d9ec3091fc60c
Koitharu <nvasya95@gmail.com>
2023-07-31 09:51:18 +0300

    Add Result utils

--- src/main/kotlin/org/koitharu/kotatsu/parsers/util/Result.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/util/Result.kt
@@ -13,3 +13,17 @@ inline fun <T, R> T.runCatchingCancellable(block: T.() -> R): Result<R> {
 		Result.failure(e)
 	}
 }
+
+inline fun <R, T : R> Result<T>.recoverCatchingCancellable(transform: (exception: Throwable) -> R): Result<R> {
+	return when (val exception = exceptionOrNull()) {
+		null -> this
+		else -> runCatchingCancellable { transform(exception) }
+	}
+}
+
+inline fun <R : Any, T : R> Result<T>.recoverNotNull(transform: (exception: Throwable) -> R?): Result<R> {
+	return when (val exception = exceptionOrNull()) {
+		null -> this
+		else -> transform(exception)?.let(Result.Companion::success) ?: this
+	}
+}
