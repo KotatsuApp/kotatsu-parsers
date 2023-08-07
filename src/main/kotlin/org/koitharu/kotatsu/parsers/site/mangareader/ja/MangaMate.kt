f34dd7926a489696c73ba338736abbf151bfdfe0
devi <github.uqmqu@aleeas.com>
2023-08-07 17:44:25 +0200

    add skanlacje-feniksy and fix hentaivn

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ja/MangaMate.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ja/MangaMate.kt
@@ -4,7 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
-import java.util.*
+import java.util.Locale
 
 @MangaSourceParser("MANGAMATE", "Manga Mate", "ja")
 internal class MangaMate(context: MangaLoaderContext) :
---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/pl/SkanlacjeFeniksy.kt
@@ -0,0 +1,13 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.pl
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("SKANLACJEFENIKSY", "SkanlacjeFeniksy", "pl")
+internal class SkanlacjeFeniksy(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.SKANLACJEFENIKSY, "skanlacje-feniksy.pl", pageSize = 10, searchPageSize = 10) {
+
+	override val datePattern = "d MMMM, yyyy"
+}
