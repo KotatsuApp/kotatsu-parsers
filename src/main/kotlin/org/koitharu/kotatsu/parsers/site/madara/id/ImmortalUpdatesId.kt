e0249accf8c35721927ab2fc593358b7c84f3512
devi <github.uqmqu@aleeas.com>
2023-08-31 09:46:19 +0200

    fix domain Mgkomik

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/ImmortalUpdatesId.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/ImmortalUpdatesId.kt
@@ -4,7 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import java.util.*
+import java.util.Locale
 
 @MangaSourceParser("IMMORTALUPDATESID", "Immortal Updates Id", "id")
 internal class ImmortalUpdatesId(context: MangaLoaderContext) :
--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/PojokManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/PojokManga.kt
@@ -4,7 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import java.util.*
+import java.util.Locale
 
 @MangaSourceParser("POJOKMANGA", "PojokManga", "id")
 internal class PojokManga(context: MangaLoaderContext) :
