27c29f99d97a53c318584a206c5537ef269de583
devi <github.uqmqu@aleeas.com>
2023-10-04 17:14:12 +0200

    minor fixes and add small sources

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/BarManga.kt
@@ -0,0 +1,12 @@
+package org.koitharu.kotatsu.parsers.site.madara.es
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+
+@MangaSourceParser("BARMANGA", "Bar Manga", "es")
+internal class BarManga(context: MangaLoaderContext) :
+	MadaraParser(context, MangaSource.BARMANGA, "barmanga.com") {
+	override val datePattern = "MM/dd/yyyy"
+}
