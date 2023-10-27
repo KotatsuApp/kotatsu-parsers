fffe104cbe91074f1c4449da8d321b475e24e807
devi <github.uqmqu@aleeas.com>
2023-10-27 18:48:33 +0200

    Minor daily update

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/Scansraw.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/Scansraw.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("SCANSRAW", "ScansRaw", "en")
+@MangaSourceParser("SCANSRAW", "AquaScans", "en")
 internal class Scansraw(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.SCANSRAW, "scansraw.com")
+	MadaraParser(context, MangaSource.SCANSRAW, "aquascans.com", 10)
