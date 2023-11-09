6bcd9dd65c704601ac8acadd7e161b57093c83f6
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2023-11-09 20:18:26 +0000

    mudou de nome

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/DemonSectParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/DemonSectParser.kt
@@ -5,8 +5,8 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("PRISMA_SCANS", "PrismaScans", "pt")
-internal class PrismaScansParser(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.PRISMA_SCANS, "prismacomics.com", 10) {
+@MangaSourceParser("DemonSect", "DemonSect", "pt")
+internal class DemonSectParser(context: MangaLoaderContext) :
+	MadaraParser(context, MangaSource.PRISMA_SCANS, "demonsect.com.br" 10) {
 	override val datePattern = "MMM dd, yyyy"
 }
