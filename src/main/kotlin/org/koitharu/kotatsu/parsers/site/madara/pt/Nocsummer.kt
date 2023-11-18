d91947ae155a1c6902fad96a75fd7e70b8d9158a
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2023-11-18 17:21:44 +0000

    NocturneSummer fix?

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Nocsummer.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Nocsummer.kt
@@ -4,7 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.model.ContentType
 
 @MangaSourceParser("NOCSUMMER", "NocturneSummer", "pt", ContentType.HENTAI)
 internal class Nocsummer(context: MangaLoaderContext) :
