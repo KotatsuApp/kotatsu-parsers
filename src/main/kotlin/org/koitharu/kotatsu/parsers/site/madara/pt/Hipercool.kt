ecfc7e40c99f1ff87eb29d51846481354c23fff5
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2023-11-18 12:29:55 +0000

    HiperCool, change url

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Hipercool.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Hipercool.kt
@@ -8,6 +8,6 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("HIPERCOOL", "Hipercool", "pt", ContentType.HENTAI)
 internal class Hipercool(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.HIPERCOOL, "hipercool.xyz", pageSize = 20) {
+	MadaraParser(context, MangaSource.HIPERCOOL, "hiper.cool", pageSize = 20) {
 	override val tagPrefix = "manga-tag/"
 }
