1c3b41acba5888b8da913bbe45f4d90c2a74d2ab
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2023-11-18 12:42:15 +0000

    Nocsummer, yaoi site, necessary (ContentType.HENTAI)

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Nocsummer.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Nocsummer.kt
@@ -5,7 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("NOCSUMMER", "NocturneSummer", "pt")
+@MangaSourceParser("NOCSUMMER", "NocturneSummer", "pt", ContentType.HENTAI)
 internal class Nocsummer(context: MangaLoaderContext) :
 	MadaraParser(context, MangaSource.NOCSUMMER, "nocsummer.com.br", 18) {
 	override val datePattern = "dd 'de' MMMMM 'de' yyyy"
