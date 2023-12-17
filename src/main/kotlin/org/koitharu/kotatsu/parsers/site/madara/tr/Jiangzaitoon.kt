eeafb3c6fd4fe5565435145e7efaa4447f376ae1
devi <github.uqmqu@aleeas.com>
2023-12-17 11:58:29 +0100

    Fix JiangzaiToon #417

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Jiangzaitoon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Jiangzaitoon.kt
@@ -9,6 +9,5 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 @MangaSourceParser("JIANGZAITOON", "JiangzaiToon", "tr", ContentType.HENTAI)
 internal class Jiangzaitoon(context: MangaLoaderContext) :
 	MadaraParser(context, MangaSource.JIANGZAITOON, "jiangzaitoon.cc") {
-	override val postReq = true
 	override val datePattern = "d MMMM yyyy"
 }
