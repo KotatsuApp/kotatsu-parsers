4e2d7398400f6df36fb615094ba71c71fdd49297
Zakhar Timoshenko <vp1984tanki@gmail.com>
2024-01-02 20:34:37 +0300

    [Shinigami] Add user-agent changing option

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/Shinigami.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/Shinigami.kt
@@ -2,14 +2,24 @@ package org.koitharu.kotatsu.parsers.site.madara.id
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.config.ConfigKey
 import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.network.UserAgents
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import java.util.Locale
+import java.util.*
 
 @MangaSourceParser("SHINIGAMI", "Shinigami", "id")
 internal class Shinigami(context: MangaLoaderContext) :
 	MadaraParser(context, MangaSource.SHINIGAMI, "shinigami.moe", 10) {
+
+	private val userAgentKey = ConfigKey.UserAgent(UserAgents.CHROME_MOBILE)
 	override val tagPrefix = "genre/"
 	override val listUrl = "series/"
 	override val sourceLocale: Locale = Locale.ENGLISH
+
+	override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
+		super.onCreateConfig(keys)
+		keys.add(userAgentKey)
+	}
+
 }
