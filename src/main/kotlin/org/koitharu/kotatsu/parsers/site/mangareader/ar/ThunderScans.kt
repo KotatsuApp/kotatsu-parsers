6ad08f13d43296c2d653922ddfa9d2a857ff02bb
devi <github.uqmqu@aleeas.com>
2024-01-09 17:51:16 +0100

    Add Comic1000, ThunderScans, WhaleManga, SussyScan, EDoujin, 3XYaoi, SussyScan #444 Url TeamXNovel, WestManga Remove AzureManga, BabelToon, FranxxMangas, LsHistoria, MangaGenki, Wowomik, MiraiScans, Nonbiri, PeaceScans, ReaperScansTr, TurkToon

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/ThunderScans.kt
@@ -0,0 +1,10 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.ar
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("THUNDERSCANS", "ThunderScans", "ar")
+internal class ThunderScans(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.THUNDERSCANS, "thunderscans.com", pageSize = 32, searchPageSize = 10)
--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/EDoujin.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/EDoujin.kt
@@ -2,9 +2,10 @@ package org.koitharu.kotatsu.parsers.site.mangareader.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@MangaSourceParser("PEACESCANS", "PeaceScans", "en")
-internal class PeaceScans(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaSource.PEACESCANS, "peacescans.com", pageSize = 14, searchPageSize = 10)
+@MangaSourceParser("EDOUJIN", "EDoujin", "en", ContentType.HENTAI)
+internal class EDoujin(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.EDOUJIN, "edoujin.net", pageSize = 25, searchPageSize = 10)
