a8d933b4f914b37c2964651c626e0b3792120e6b
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2024-01-19 18:02:56 +0000

    RandomScans -> Luratoon

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Luratoon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Luratoon.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("RANDOMSCANS", "RandomScans", "pt")
-internal class RandomScans(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.RANDOMSCANS, "randomscanlators.net")
+@MangaSourceParser("LURATOON", "Luratoon", "pt")
+internal class LURATOON(context: MangaLoaderContext) :
+	MadaraParser(context, MangaSource.LURATOON, "luratoon.com")
