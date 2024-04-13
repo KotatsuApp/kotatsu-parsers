1bee40c0006b8f12c85d966babc747119213cfda
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-04-13 16:53:34 +0500

    MangaGalaxy: move to MangaReader

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/MangaGalaxy.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/MangaGalaxy.kt
@@ -1,13 +1,12 @@
-package org.koitharu.kotatsu.parsers.site.madara.en
+package org.koitharu.kotatsu.parsers.site.mangareader.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
-import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
 @MangaSourceParser("MANGAGALAXY", "MangaGalaxy", "en")
 internal class MangaGalaxy(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.MANGAGALAXY, "mangagalaxy.me", 16) {
-
-	override val datePattern = "MM/dd/yyyy"
+	MangaReaderParser(context, MangaSource.MANGAGALAXY, "mangagalaxy.me", 20, 16) {
+		override val listUrl = "/series"
 }
