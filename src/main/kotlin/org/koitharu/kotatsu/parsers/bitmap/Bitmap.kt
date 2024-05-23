4edcd7087102bcd6c5c9194dd6ff95ce84f7bad8
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-05-23 11:51:00 +0500

    simplify api

--- src/main/kotlin/org/koitharu/kotatsu/parsers/bitmap/Bitmap.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/bitmap/Bitmap.kt
@@ -1,53 +1,8 @@
 package org.koitharu.kotatsu.parsers.bitmap
 
-import kotlinx.io.Sink
+interface Bitmap {
+	val width: Int
+	val height: Int
 
-abstract class Bitmap {
-	abstract val width: Int
-	abstract val height: Int
-
-	enum class CompressFormat {
-		JPEG,
-		PNG,
-		WEBP_LOSSY,
-		WEBP_LOSSLESS,
-	}
-
-	enum class Config {
-		ALPHA_8,
-		RGB_565,
-		ARGB_8888,
-		RGBA_F16,
-		HARDWARE,
-		RGBA_1010102,
-	}
-
-	abstract fun compress(
-		format: CompressFormat,
-		quality: Int,
-		sink: Sink
-	): Boolean
-
-	abstract fun getPixels(
-		/*@ColorInt*/
-		pixels: IntArray,
-		offset: Int,
-		stride: Int,
-		x: Int,
-		y: Int,
-		width: Int,
-		height: Int,
-	)
-
-	abstract fun drawBitmap(sourceBitmap: Bitmap, src: Rect, dst: Rect)
-
-	companion object {
-		fun createBitmap(width: Int, height: Int, config: Config): Bitmap {
-			throw NotImplementedError("createBitmap(width, height, config) is not implemented")
-		}
-
-		fun createBitmap(source: Bitmap, x: Int, y: Int, width: Int, height: Int): Bitmap {
-			throw NotImplementedError("createBitmap(source, x, y, width, height) is not implemented")
-		}
-	}
+	fun drawBitmap(sourceBitmap: Bitmap, src: Rect, dst: Rect)
 }
