c3ff1de1c45fcf5c0d958704d446cb286022edfb
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-27 14:24:54 +0300

    Update and rename Arabic

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LinkManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LinkManga.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("MANGALINKNET", "MangaLink.net", "ar")
+@MangaSourceParser("LINKMANGA", "LinkManga.com", "ar")
 internal class MangalinkNet(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.MANGALINKNET, "manga-link.net", pageSize = 10)
+	MadaraParser(context, MangaSource.MANGALINKNET, "link-manga.com", pageSize = 10)
