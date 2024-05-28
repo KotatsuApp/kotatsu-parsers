2a627fa24d1d35c682b5cf5cb42f4ca672125cea
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-28 22:36:48 +0300

    Update and rename LikeManga.kt to LikeMangaNet.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LikeMangaNet.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LikeMangaNet.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("LIKEMANGA", "Like-Manga.net", "ar")
-internal class MangaLike(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.LIKEMANGA, "like-manga.net", pageSize = 10)
+@MangaSourceParser("LIKEMANGANET", "Like-Manga.net", "ar")
+internal class MangaLikeNet(context: MangaLoaderContext) :
+	MadaraParser(context, MangaSource.LIKEMANGANET, "like-manga.net", pageSize = 10)
