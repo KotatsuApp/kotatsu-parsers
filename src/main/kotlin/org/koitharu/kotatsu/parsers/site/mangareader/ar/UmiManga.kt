528fadceb857ca8ac6e1faf8da1dff8df537068d
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-28 22:10:40 +0300

    Update UmiManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/UmiManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/UmiManga.kt
@@ -5,7 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@MangaSourceParser("UmiManga", "UmiManga", "ar")
+@MangaSourceParser("UMIMANGA", "UmiManga", "ar")
 internal class BeastScans(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaSource.UMIMANGA, "www.umimanga.com", pageSize = 20, searchPageSize = 10) {
 	override val isTagsExclusionSupported = false
