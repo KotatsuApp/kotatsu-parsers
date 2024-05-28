77a733a0628930d7b0bea710c85665fab8f216f6
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-28 23:24:55 +0300

    Update and rename EnAresManga.kt to FlAres.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/FlAres.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/FlAres.kt
@@ -5,9 +5,9 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@MangaSourceParser("ENARESMANGA", "En-AresManga", "ar")
-internal class EnAresManga(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaSource.ENARESMANGA, "en-aresmanga.com", pageSize = 20, searchPageSize = 10) {
+@MangaSourceParser("FLARES", "Fl-Ares", "ar")
+internal class FlAres(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.FLARES, "fl-ares.com", pageSize = 20, searchPageSize = 10) {
 	override val listUrl = "/series"
 	override val encodedSrc = true
 	override val isTagsExclusionSupported = false
