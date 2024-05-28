b1effdb7c77510434df9bbdaf484e891ba09a084
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-28 21:42:23 +0300

    Update and rename Azoranov.kt to AzoraMoon.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AzoraMoon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AzoraMoon.kt
@@ -5,9 +5,9 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("AZORANOV", "AzoraMoon", "ar")
-internal class Azoranov(context: MangaLoaderContext) :
-	MadaraParser(context, MangaSource.AZORANOV, "azoramoon.com", pageSize = 10) {
+@MangaSourceParser("AZORAMoon", "AzoraMoon", "ar")
+internal class AzoraMoon(context: MangaLoaderContext) :
+	MadaraParser(context, MangaSource.AZORAMOON, "azoramoon.com", pageSize = 10) {
 	override val tagPrefix = "series-genre/"
 	override val listUrl = "series/"
 }
