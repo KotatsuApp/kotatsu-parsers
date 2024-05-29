82f24114a83b0a5bd3573e446a213a3ee7208f9a
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-05-29 08:05:51 +0300

    Update and rename KingOfShojo.kt to Hijalacom.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/Hijalacom.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/Hijalacom.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@MangaSourceParser("KINGOFSHOJO", "KingOfShojo", "ar")
-internal class KingOfShojo(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaSource.KINGOFSHOJO, "kingofshojo.com", pageSize = 30, searchPageSize = 10)
+@MangaSourceParser("HIJALACOM", "Hijalacom", "ar")
+internal class Hijalacom(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaSource.HIJALACOM, "hijala.com", pageSize = 30, searchPageSize = 10)
