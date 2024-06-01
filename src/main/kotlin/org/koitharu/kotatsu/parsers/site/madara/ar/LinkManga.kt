e7826c2570f7f9b5bb8d66604df51fdd7af90904
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-06-01 12:26:43 +0300

    Update src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LinkManga.kt
    Co-authored-by: Koitharu <nvasya95@gmail.com>

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LinkManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/LinkManga.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("LINKMANGA", "Link-Manga.com", "ar")
+@MangaSourceParser("MANGALINKNET", "Link-Manga.com", "ar")
 internal class LinkManga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaSource.LINKMANGA, "link-manga.com", pageSize = 10)
