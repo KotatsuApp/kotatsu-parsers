2d1899f1f229d2a0c4646f973edf5f4c5c0767a6
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-06-01 09:03:12 +0300

    Update TeamXNovel.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/ar/TeamXNovel.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/ar/TeamXNovel.kt
@@ -20,7 +20,7 @@ internal class TeamXNovel(context: MangaLoaderContext) : PagedMangaParser(contex
 	override val availableStates: Set<MangaState> =
 		EnumSet.of(MangaState.ONGOING, MangaState.FINISHED, MangaState.ABANDONED)
 
-	override val configKeyDomain = ConfigKey.Domain("team11x11.com")
+	override val configKeyDomain = ConfigKey.Domain("teamxnovel.com")
 	override val isMultipleTagsSupported = false
 
 	override suspend fun getListPage(page: Int, filter: MangaListFilter?): List<Manga> {
