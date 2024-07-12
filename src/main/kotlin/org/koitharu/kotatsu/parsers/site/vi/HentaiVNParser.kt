cf5abdf7d3be5569bf2e693e9b9e13b35a426ff8
Draken <131387159+dragonx943@users.noreply.github.com>
2024-07-12 16:22:44 +0700

    Update HentaiVNParser.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/HentaiVNParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/HentaiVNParser.kt
@@ -23,7 +23,7 @@ private const val SEARCH_PAGE_SIZE = 10
 @MangaSourceParser("HENTAIVN", "HentaiVN", "vi", type = ContentType.HENTAI)
 class HentaiVNParser(context: MangaLoaderContext) : MangaParser(context, MangaSource.HENTAIVN) {
 
-	override val configKeyDomain: ConfigKey.Domain = ConfigKey.Domain("zhentaivn.cc", "hentaivn.bio")
+	override val configKeyDomain: ConfigKey.Domain = ConfigKey.Domain("zhentaivnz.cc", "hentaivn.homes")
 
 	// hentaivn has created 2 different interfaces for mobile and desktop, and Cloudflare detects whether it's mobile or not even with a desktop user agent.
 	override val headers: Headers = Headers.Builder().add("User-Agent", UserAgents.FIREFOX_DESKTOP).build()
