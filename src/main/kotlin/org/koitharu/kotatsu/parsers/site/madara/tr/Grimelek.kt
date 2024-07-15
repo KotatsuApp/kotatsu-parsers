35bdc15aa9bede84317e7cd42773cec0cbf48c2b
devi <github.uqmqu@aleeas.com>
2024-07-15 18:49:19 +0200

    Fix url grimelek

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Grimelek.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Grimelek.kt
@@ -3,13 +3,12 @@ package org.koitharu.kotatsu.parsers.site.madara.tr
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
-import org.koitharu.kotatsu.parsers.model.MangaSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 //This source requires an account.
 @MangaSourceParser("GRIMELEK", "Grimelek", "tr")
 internal class Grimelek(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.GRIMELEK, "glorymanga.com", 20) {
+	MadaraParser(context, MangaParserSource.GRIMELEK, "grimelek.org", 20) {
 	override val datePattern = "d MMMM yyyy"
 	override val listUrl = "seri/"
 }
