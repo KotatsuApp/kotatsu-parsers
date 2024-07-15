e18787575edd4d581cc98c2d81797ae488c47e29
Inkesk <djdomp0@gmail.com>
2024-07-16 00:39:22 +0530

    Update DarkScans.kt
    Updated the domain name from darkscans.com to darkscans.net

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/DarkScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/DarkScans.kt
@@ -7,7 +7,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("DARK_SCANS", "DarkScans", "en")
 internal class DarkScans(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.DARK_SCANS, "darkscans.com", 18) {
+	MadaraParser(context, MangaParserSource.DARK_SCANS, "darkscans.net", 18) {
 	override val listUrl = "mangas/"
 	override val tagPrefix = "mangas-genre/"
 }
