035a521409aebbd33726e17d793feb82ddb79fb6
Draken <131387159+dragonx943@users.noreply.github.com>
2024-07-29 19:19:06 +0700

    Update NetTruyen.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/NetTruyen.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/NetTruyen.kt
@@ -8,7 +8,7 @@ import org.koitharu.kotatsu.parsers.site.wpcomics.WpComicsParser
 
 @MangaSourceParser("NETTRUYEN", "NetTruyen", "vi")
 internal class NetTruyen(context: MangaLoaderContext) :
-	WpComicsParser(context, MangaParserSource.NETTRUYEN, "nettruyenaa.com", 36) {
+	WpComicsParser(context, MangaParserSource.NETTRUYEN, "nettruyenaa.com") {
 	override val configKeyDomain: ConfigKey.Domain = ConfigKey.Domain(
 		"nettruyenssr.com",
 	)
