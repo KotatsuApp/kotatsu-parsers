726ecf77fd347f8c476c3583f6e001e87e7cd417
Draken <131387159+dragonx943@users.noreply.github.com>
2024-09-06 11:38:11 +0700

    Update TruyenVn Domain

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/TruyenVn.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/TruyenVn.kt
@@ -8,7 +8,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("TRUYENVN", "TruyenVn", "vi", ContentType.HENTAI)
 internal class TruyenVn(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.TRUYENVN, "truyenvn.cam", 20) {
+	MadaraParser(context, MangaParserSource.TRUYENVN, "truyenvn.mobi", 20) {
 	override val listUrl = "truyen-tranh/"
 	override val tagPrefix = "the-loai/"
 	override val datePattern = "dd/MM/yyyy"
