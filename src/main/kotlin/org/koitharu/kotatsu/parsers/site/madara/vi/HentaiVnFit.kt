0bafd117ffbd63e92d9d2d8ad7294c7763697703
Draken <131387159+dragonx943@users.noreply.github.com>
2024-09-18 11:17:49 +0700

    Update and rename HentaiVnCafe.kt to HentaiVnFit.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/HentaiVnFit.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/vi/HentaiVnFit.kt
@@ -6,9 +6,9 @@ import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("HENTAIVNCAFE", "HentaiVnCafe", "vi", ContentType.HENTAI)
+@MangaSourceParser("HENTAIVNFIT", "HentaiVnFit", "vi", ContentType.HENTAI)
 internal class HentaiVnCafe(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.HENTAIVNCAFE, "hentaivn.cafe", 24) {
+	MadaraParser(context, MangaParserSource.HENTAIVNFIT, "hentaivn.fit", 24) {
 	override val listUrl = "truyen-hentai/"
 	override val tagPrefix = "the-loai/"
 	override val datePattern = "dd/MM/yyyy"
