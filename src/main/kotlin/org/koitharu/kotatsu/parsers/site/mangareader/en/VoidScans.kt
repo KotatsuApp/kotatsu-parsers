1c0df02c567bf3a1ec64c1bc99148e31723e066f
devi <github.uqmqu@aleeas.com>
2024-09-24 19:39:58 +0200

    [HiveToon] fix close #1086

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/VoidScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/en/VoidScans.kt
@@ -2,16 +2,9 @@ package org.koitharu.kotatsu.parsers.site.mangareader.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
-import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
 @MangaSourceParser("VOIDSCANS", "HiveToon", "en")
 internal class VoidScans(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.VOIDSCANS, "hivetoon.com", pageSize = 150, searchPageSize = 150) {
-	override val datePattern = "MMM d, yyyy"
-	override val filterCapabilities: MangaListFilterCapabilities
-		get() = super.filterCapabilities.copy(
-			isTagsExclusionSupported = false,
-		)
-}
+	MangaReaderParser(context, MangaParserSource.VOIDSCANS, "hivetoon.net", pageSize = 15, searchPageSize = 10)
