6ec65e4aa44e9c1ce54e8f620e0650766b993a10
Draken <131387159+dragonx943@users.noreply.github.com>
2024-10-01 13:08:23 +0700

    Update TopTruyenViet.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/TopTruyenViet.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/TopTruyenViet.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.wpcomics.WpComicsParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken
 @MangaSourceParser("TOPTRUYENVIET", "TopTruyen.pro", "vi")
 internal class TopTruyenViet(context: MangaLoaderContext) :
 	WpComicsParser(context, MangaParserSource.TOPTRUYENVIET, "www.toptruyenzz.pro", 36)
