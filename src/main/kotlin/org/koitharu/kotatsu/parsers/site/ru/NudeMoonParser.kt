5c55d65eb3835554e46709174e3ecdfe3995a415
Koitharu <nvasya95@gmail.com>
2024-10-09 08:18:29 +0300

    Refactor and fixes

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/ru/NudeMoonParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/ru/NudeMoonParser.kt
@@ -17,6 +17,7 @@ internal class NudeMoonParser(
 ) : MangaParser(context, MangaParserSource.NUDEMOON), MangaParserAuthProvider {
 
 	override val configKeyDomain = ConfigKey.Domain(
+		"b.nude-moon.fun",
 		"x.nude-moon.fun",
 		"nude-moon.org",
 		"nude-moon.net",
--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/TopTruyenViet.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/TopTruyenViet.kt
@@ -1,18 +1,17 @@
 package org.koitharu.kotatsu.parsers.site.wpcomics.vi
 
 import org.jsoup.nodes.Document
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.config.ConfigKey
 import org.koitharu.kotatsu.parsers.model.MangaChapter
 import org.koitharu.kotatsu.parsers.model.MangaPage
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.network.UserAgents
 import org.koitharu.kotatsu.parsers.site.wpcomics.WpComicsParser
 import org.koitharu.kotatsu.parsers.util.*
 import java.text.SimpleDateFormat
-import java.util.*
-import org.koitharu.kotatsu.parsers.config.ConfigKey
-import org.koitharu.kotatsu.parsers.network.UserAgents
-import org.koitharu.kotatsu.parsers.Broken
 
 @Broken
 @MangaSourceParser("TOPTRUYENVIET", "TopTruyen.pro", "vi")
@@ -25,45 +24,42 @@ internal class TopTruyenViet(context: MangaLoaderContext) :
 		super.onCreateConfig(keys)
 		keys.add(userAgentKey)
 	}
-	
-    override val selectPage = "div.page-chapter > img, div.chapter-content > img"
-    override val selectChapter = "div.list-chapter > a"
-    override val selectDate = "div.col-xs-4"
-    override val datePattern = "dd/MM/yyyy"
 
-    override suspend fun getChapters(doc: Document): List<MangaChapter> {
-        return doc.select(selectChapter).mapChapters(reversed = true) { i, a ->
-            val href = a.attrAsRelativeUrl("href")
-            val dateText = a.selectFirst(selectDate)?.text()
-            val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
-            MangaChapter(
-                id = generateUid(href),
-                name = a.text(),
-                number = i + 1f,
-                url = href,
-                uploadDate = parseChapterDate(
-                    dateFormat,
-                    dateText,
-                ),
-                source = source,
-                scanlator = null,
-                branch = null,
-                volume = 0,
-            )
-        }
-    }
+	override val selectPage = "div.page-chapter > img, div.chapter-content > img"
+	override val selectChapter = "div.list-chapter > a"
+	override val selectDate = "div.col-xs-4"
+	override val datePattern = "dd/MM/yyyy"
 
-    override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
-        val fullUrl = chapter.url.toAbsoluteUrl(domain)
-        val doc = webClient.httpGet(fullUrl).parseHtml()
-        return doc.select(selectPage).map { img ->
-            val url = img.src()?.toRelativeUrl(domain) ?: img.parseFailed("Image src not found")
-            MangaPage(
-                id = generateUid(url),
-                url = url,
-                preview = null,
-                source = source,
-            )
-        }
-    }
+	override suspend fun getChapters(doc: Document): List<MangaChapter> {
+		val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
+		return doc.select(selectChapter).mapChapters(reversed = true) { i, a ->
+			val href = a.attrAsRelativeUrl("href")
+			val dateText = a.selectFirst(selectDate)?.text()
+			MangaChapter(
+				id = generateUid(href),
+				name = a.text(),
+				number = i + 1f,
+				url = href,
+				uploadDate = parseChapterDate(dateFormat, dateText),
+				source = source,
+				scanlator = null,
+				branch = null,
+				volume = 0,
+			)
+		}
+	}
+
+	override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
+		val fullUrl = chapter.url.toAbsoluteUrl(domain)
+		val doc = webClient.httpGet(fullUrl).parseHtml()
+		return doc.select(selectPage).map { img ->
+			val url = img.src()?.toRelativeUrl(domain) ?: img.parseFailed("Image src not found")
+			MangaPage(
+				id = generateUid(url),
+				url = url,
+				preview = null,
+				source = source,
+			)
+		}
+	}
 }
