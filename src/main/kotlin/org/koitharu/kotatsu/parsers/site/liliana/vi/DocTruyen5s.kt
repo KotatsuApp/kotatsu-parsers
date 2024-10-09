3fe49ea3407fababdec7e29a3158fa36d75a1aa8
Draken <131387159+dragonx943@users.noreply.github.com>
2024-10-09 22:06:05 +0700

    Update DocTruyen5s.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
@@ -7,9 +7,4 @@ import org.koitharu.kotatsu.parsers.site.liliana.LilianaParser
 
 @MangaSourceParser("DOCTRUYEN5S", "DocTruyen5s", "vi")
 internal class DocTruyen5s(context: MangaLoaderContext) :
-    LilianaParser(context, MangaParserSource.DOCTRUYEN5S, "dongmoe.com") {
-
-    override suspend fun getAvailableTags() = super.getAvailableTags()
-    override suspend fun getFilterOptions() = super.getFilterOptions()
-    
-}
+    LilianaParser(context, MangaParserSource.DOCTRUYEN5S, "dongmoe.com")
