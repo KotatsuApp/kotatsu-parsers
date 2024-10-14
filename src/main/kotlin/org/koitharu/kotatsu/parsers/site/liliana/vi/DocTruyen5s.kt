82fb0379f62933f5e8cfd02e2a928b5c64c91488
Draken <131387159+dragonx943@users.noreply.github.com>
2024-10-14 18:41:08 +0700

    Update DocTruyen5s.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.liliana.LilianaParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken
 @MangaSourceParser("DOCTRUYEN5S", "DocTruyen5s", "vi")
 internal class DocTruyen5s(context: MangaLoaderContext) :
     LilianaParser(context, MangaParserSource.DOCTRUYEN5S, "dongmoe.com")
