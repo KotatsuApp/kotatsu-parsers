7ce2a97c1f1c0bfaff26649ce7fbe61eaae91782
Koitharu <nvasya95@gmail.com>
2024-10-17 17:39:47 +0300

    [DocTruyen5s] Refactoring

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/liliana/vi/DocTruyen5s.kt
@@ -8,41 +8,39 @@ import org.koitharu.kotatsu.parsers.model.MangaPage
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.liliana.LilianaParser
 import org.koitharu.kotatsu.parsers.util.*
+import org.koitharu.kotatsu.parsers.util.json.getBooleanOrDefault
 
 @MangaSourceParser("DOCTRUYEN5S", "DocTruyen5s", "vi")
 internal class DocTruyen5s(context: MangaLoaderContext) :
-	LilianaParser(context, MangaParserSource.DOCTRUYEN5S, "dongmoe.com") {
+	LilianaParser(context, MangaParserSource.DOCTRUYEN5S, "dongmoe.com", pageSize = 42) {
 
-    override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
-        val fullUrl = chapter.url.toAbsoluteUrl(domain)
-        val doc = webClient.httpGet(fullUrl).parseHtml()
-        val script = doc.selectFirstOrThrow("script:containsData(const CHAPTER_ID)")?.data()
-        val chapterId = script?.substringAfter("const CHAPTER_ID = ")?.substringBefore(';')
-            ?: throw IllegalStateException("Không thể tìm thấy CHAPTER_ID, hãy kiểm tra nguồn")
+	override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
+		val fullUrl = chapter.url.toAbsoluteUrl(domain)
+		val doc = webClient.httpGet(fullUrl).parseHtml()
+		val script = doc.selectFirstOrThrow("script:containsData(const CHAPTER_ID)").data()
+		val chapterId = script.substringAfter("const CHAPTER_ID = ", "").substringBefore(';', "")
+		check(chapterId.isNotEmpty()) { "Không thể tìm thấy CHAPTER_ID, hãy kiểm tra nguồn" }
 
-        val ajaxUrl = buildString {
-            append("https://")
-            append(domain)
-            append("/ajax/image/list/chap/")
-            append(chapterId)
-        }
+		val ajaxUrl = buildString {
+			append("https://")
+			append(domain)
+			append("/ajax/image/list/chap/")
+			append(chapterId)
+		}
 
-        val responseJson = webClient.httpGet(ajaxUrl).parseJson()
+		val responseJson = webClient.httpGet(ajaxUrl).parseJson()
+		check(responseJson.getBooleanOrDefault("status", false)) { responseJson.getString("msg") }
 
-        if (!responseJson.optBoolean("status", false)) {
-            throw IllegalStateException(responseJson.optString("msg"))
-        }
+		val pageListDoc = Jsoup.parse(responseJson.getString("html"))
 
-        val pageListDoc = Jsoup.parse(responseJson.getString("html"))
-
-        return pageListDoc.select("div.separator a").map { element ->
-            val url = element.attr("href")
-            MangaPage(
-                id = generateUid(url),
-                url = url,
-                preview = null,
-                source = source
-            )
-        }
-    }
+		return pageListDoc.selectOrThrow("div.separator a").map { element ->
+			val url = element.attr("href")
+			MangaPage(
+				id = generateUid(url),
+				url = url,
+				preview = null,
+				source = source,
+			)
+		}
+	}
 }
