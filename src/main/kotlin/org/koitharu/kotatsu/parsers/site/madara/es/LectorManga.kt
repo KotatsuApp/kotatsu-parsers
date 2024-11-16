f6096bbe65c710e64e5ff2de7e8104fb6cc89376
Draken <131387159+dragonx943@users.noreply.github.com>
2024-11-17 01:52:14 +0700

    Update LectorManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/LectorManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/LectorManga.kt
@@ -7,7 +7,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("LECTORMANGA", "LectorManga", "es")
 internal class LectorManga(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.LECTORMANGA, "www.lectormanga.lat") {
+	MadaraParser(context, MangaParserSource.LECTORMANGA, "lectormangaa.com") {
 	override val listUrl = "biblioteca/"
 	override val tagPrefix = "comics-genero/"
 }
