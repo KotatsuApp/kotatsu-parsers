87f1cd8cdc53b1e75446e9cf690d0a617275fc5b
Draken <131387159+dragonx943@users.noreply.github.com>
2025-01-29 22:29:15 +0700

    Update Komiku.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Komiku.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Komiku.kt
@@ -8,7 +8,7 @@ import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
 @MangaSourceParser("KOMIKU", "Komiku", "id")
 internal class Komiku(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.KOMIKU, "komiku.com", pageSize = 20, searchPageSize = 20) {
+	MangaReaderParser(context, MangaParserSource.KOMIKU, "komiku.one", pageSize = 20, searchPageSize = 20) {
 	override val datePattern = "MMM d, yyyy"
 	override val filterCapabilities: MangaListFilterCapabilities
 		get() = super.filterCapabilities.copy(
