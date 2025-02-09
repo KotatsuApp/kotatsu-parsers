d5a299d7ffa83fae24ce939458f25554f4b3497c
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-02-09 19:05:11 +0500

    mark MangaSee and MangaLife as broken

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/nepnep/en/Manga4Life.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/nepnep/en/Manga4Life.kt
@@ -1,10 +1,13 @@
 package org.koitharu.kotatsu.parsers.site.nepnep.en
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.nepnep.NepnepParser
 
+// site closed in favour of weeb central
+@Broken
 @MangaSourceParser("MANGA4LIFE", "Manga4Life", "en")
 internal class Manga4Life(context: MangaLoaderContext) :
 	NepnepParser(context, MangaParserSource.MANGA4LIFE, "manga4life.com")
