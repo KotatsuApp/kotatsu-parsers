76857ff6826aa9191b97bfbe4e707030ca6de2ff
bai <bai@baturax.com>
2025-02-26 00:14:56 +0300

    change site name

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/YetiskinRuyaManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/YetiskinRuyaManga.kt
@@ -5,7 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@MangaSourceParser("YETISKINRUYAMANGA", "YaoiMangaOku", "tr")
+@MangaSourceParser("YETISKINRUYAMANGA", "Yetişkin Rüya Manga", "tr")
 internal class YetiskinRuyaManga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.YETISKINRUYAMANGA, "www.yetiskinruyamanga.com", 16) {
 	override val datePattern = "dD/MM/yyyy"
