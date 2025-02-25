3d9b16da63ac53aa4f117aa3225535f68473ee6d
bai <bai@baturax.com>
2025-02-26 00:26:19 +0300

    rüyamange new domain

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/RuyaManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/RuyaManga.kt
@@ -7,7 +7,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("RUYAMANGA", "RuyaManga", "tr")
 internal class RuyaManga(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.RUYAMANGA, "www.ruya-manga.com", 18) {
+	MadaraParser(context, MangaParserSource.RUYAMANGA, "ruyamanga.net", 18) {
 	override val tagPrefix = "manga-kategori/"
 	override val datePattern = "dd/MM/yyyy"
 }
