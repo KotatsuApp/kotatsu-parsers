f28cd3aad9c90de33b4726b1c9d0f915f19067f8
bai <bai@baturax.com>
2025-02-26 00:17:20 +0300

    webtoonhatti new domain

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Webtoonhatti.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Webtoonhatti.kt
@@ -7,7 +7,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("WEBTOONHATTI", "WebtoonHatti", "tr")
 internal class Webtoonhatti(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.WEBTOONHATTI, "webtoonhatti.dev", 20) {
+	MadaraParser(context, MangaParserSource.WEBTOONHATTI, "webtoonhatti.me", 20) {
 	override val listUrl = "webtoon/"
 	override val tagPrefix = "webtoon-tur/"
 	override val datePattern = "d MMMM"
