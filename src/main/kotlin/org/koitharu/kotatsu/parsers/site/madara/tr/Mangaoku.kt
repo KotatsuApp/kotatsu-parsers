b6b310dbd69776c8c115bb0b9c6edcdda12dc40b
Draken <131387159+dragonx943@users.noreply.github.com>
2025-02-26 18:40:08 +0700

    Update Mangaoku.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Mangaoku.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Mangaoku.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+import org.koitharu.kotatsu.parsers.Broken
 
 @Broken
 @MangaSourceParser("MANGAOKU", "Mangaoku", "tr")
