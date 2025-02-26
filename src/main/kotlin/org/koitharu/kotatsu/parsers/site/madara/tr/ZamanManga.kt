6b4a6006513e244effbaef6e9a78b2bf9f797590
Draken <131387159+dragonx943@users.noreply.github.com>
2025-02-26 19:01:17 +0700

    Revert "zamanmanga is closed"
    This reverts commit 6281d770c1323924b255e9ff488667c012d06e98.

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/ZamanManga.kt
@@ -0,0 +1,14 @@
+package org.koitharu.kotatsu.parsers.site.madara.tr
+
+import org.koitharu.kotatsu.parsers.Broken
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+
+@Broken
+@MangaSourceParser("ZAMANMANGA", "ZamanManga", "tr")
+internal class ZamanManga(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.ZAMANMANGA, "zamanmanga.com") {
+	override val datePattern = "dd MMMM yyyy"
+}
