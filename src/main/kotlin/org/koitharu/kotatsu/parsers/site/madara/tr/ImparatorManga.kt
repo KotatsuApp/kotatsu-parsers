c4be0b58caad94cecbc755316b1846bef82f311e
Draken <131387159+dragonx943@users.noreply.github.com>
2025-02-26 18:39:42 +0700

    Update ImparatorManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/ImparatorManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/ImparatorManga.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken
 @MangaSourceParser("IMPARATORMANGA", "ImparatorManga", "tr")
 internal class ImparatorManga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.IMPARATORMANGA, "www.imparatormanga.com")
