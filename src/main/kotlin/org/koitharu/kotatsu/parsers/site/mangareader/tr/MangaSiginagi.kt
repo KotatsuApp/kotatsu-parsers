e7cb83f7b1df37d6b784d3706b3a894f421470b3
Draken <131387159+dragonx943@users.noreply.github.com>
2025-02-26 18:44:43 +0700

    Update MangaSiginagi.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/MangaSiginagi.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/MangaSiginagi.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken
 @MangaSourceParser("MANGASIGINAGI", "MangaSiginagi", "tr")
 internal class MangaSiginagi(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.MANGASIGINAGI, "mangasiginagi.com", pageSize = 20, searchPageSize = 10)
