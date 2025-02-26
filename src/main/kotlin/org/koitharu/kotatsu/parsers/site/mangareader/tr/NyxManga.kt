ed17c18554cdff71557dcdccd335cd112f541af5
Draken <131387159+dragonx943@users.noreply.github.com>
2025-02-26 19:01:17 +0700

    Revert "nyx mange closed"
    This reverts commit b95bb46d0004c4a157215405f7f3e6cda21d8629.

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/NyxManga.kt
@@ -0,0 +1,19 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.tr
+
+import org.koitharu.kotatsu.parsers.Broken
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@Broken
+@MangaSourceParser("NYXMANGA", "NyxManga", "tr")
+internal class NyxManga(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.NYXMANGA, "nyxmanga.com", pageSize = 14, searchPageSize = 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = false,
+		)
+}
+
