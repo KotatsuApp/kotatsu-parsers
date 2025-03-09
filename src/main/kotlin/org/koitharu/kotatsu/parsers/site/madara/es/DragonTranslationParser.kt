02359a263089e4a77443f7877625f140355b50d6
Draken <131387159+dragonx943@users.noreply.github.com>
2025-03-09 22:53:11 +0700

    Fix build

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/DragonTranslationParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/DragonTranslationParser.kt
@@ -1,6 +1,6 @@
 package org.koitharu.kotatsu.parsers.site.madara.es
 
-import org.koitharu.kotatsu.parsers.broken
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.*
