6ed42bc28b693286c1aa1ea2792b211957c46ddc
dragonx943 <premieregirl26@gmail.com>
2025-03-14 00:58:02 +0700

    [Nhentai World] Fix chapters list

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/NhentaiWorld.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/NhentaiWorld.kt
@@ -8,7 +8,6 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.config.ConfigKey
 import org.koitharu.kotatsu.parsers.core.LegacyPagedMangaParser
-import org.koitharu.kotatsu.parsers.exception.ParseException
 import org.koitharu.kotatsu.parsers.model.*
 import org.koitharu.kotatsu.parsers.util.*
 import org.koitharu.kotatsu.parsers.util.json.getStringOrNull
