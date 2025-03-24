2463477654c9cad027e4f28ede320494caeb7768
dragonx943 <premieregirl26@gmail.com>
2025-03-24 17:48:22 +0700

    [CuuTruyen] Fix search with state + Tags

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/CuuTruyenParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/CuuTruyenParser.kt
@@ -76,6 +76,16 @@ internal class CuuTruyenParser(context: MangaLoaderContext) :
 					if (tag != null) {
 						append("/api/v2/tags/")
 						append(tag.key)
+					} else if (filter.states.isNotEmpty()) {
+						filter.states.oneOrThrowIfMany()?.let {
+							append(
+								when (it) {
+									MangaState.ONGOING -> "/api/v2/tags/dang-tien-hanh"
+									MangaState.FINISHED -> "/api/v2/tags/da-hoan-thanh"
+									else -> "/api/v2/mangas/recently_updated" // if not (default page)
+								}
+							)
+						}
 					} else {
 						append("/api/v2/mangas")
 						when (order) {
@@ -145,8 +155,7 @@ internal class CuuTruyenParser(context: MangaLoaderContext) :
 
 		val state = when {
 			tags.any { it.key == "da-hoan-thanh" } -> MangaState.FINISHED
-			tags.any { it.key == "dang-tien-hanh" } -> MangaState.ONGOING
-			else -> null
+			else -> MangaState.ONGOING // Mostly ONGOING but not marked by site owner
 		}
 
 		val newTags = tags.filter { it.key != "da-hoan-thanh" && it.key != "dang-tien-hanh" }.toSet()
@@ -260,7 +269,6 @@ internal class CuuTruyenParser(context: MangaLoaderContext) :
 		MangaTag("Gyaru", "gyaru", source),
 		MangaTag("4-Koma", "4-koma", source),
 		MangaTag("Manga", "manga", source),
-		MangaTag("Đang tiến hành", "dang-tien-hanh", source),
 		MangaTag("Thể thao", "the-thao", source),
 		MangaTag("Hài hước", "hai-huoc", source),
 		MangaTag("Shounen", "shounen", source),
@@ -279,7 +287,6 @@ internal class CuuTruyenParser(context: MangaLoaderContext) :
 		MangaTag("Phiêu lưu", "phieu-luu", source),
 		MangaTag("Hậu tận thế", "hau-tan-the", source),
 		MangaTag("Hành động", "hanh-dong", source),
-		MangaTag("Đã hoàn thành", "da-hoan-thanh", source),
 		MangaTag("Sinh tồn", "sinh-ton", source),
 		MangaTag("Du hành thời gian", "du-hanh-thoi-gian", source),
 		MangaTag("Khoa học", "khoa-hoc", source),
