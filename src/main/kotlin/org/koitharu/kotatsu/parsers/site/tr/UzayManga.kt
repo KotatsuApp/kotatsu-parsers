8105523eb65844c03ac3be162779c67137cc5866
Draken <131387159+dragonx943@users.noreply.github.com>
2025-04-26 21:50:18 +0700

    [UzayManga] Clean up

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/tr/UzayManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/tr/UzayManga.kt
@@ -22,13 +22,11 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
 
     override val filterCapabilities: MangaListFilterCapabilities
         get() = MangaListFilterCapabilities(
-            isMultipleTagsSupported = true,
-            isTagsExclusionSupported = false,
             isSearchSupported = true,
+            isMultipleTagsSupported = true,
         )
 
     override suspend fun getFilterOptions() = MangaListFilterOptions(
-        availableTags = fetchAvailableTags(),
         availableStates = EnumSet.of(MangaState.ONGOING, MangaState.FINISHED),
         availableContentTypes = EnumSet.of(
             ContentType.MANGA,
@@ -41,10 +39,17 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
 
     override suspend fun getListPage(page: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
         val url = buildString {
-            append("https://uzaymanga.com/search?page=$page&search=")
+            append("https://")
+			append(domain)
+            append("/search")
+            append("&page=")
+			append(page.toString())
+
             if (!filter.query.isNullOrEmpty()) {
+                append("&search")
                 append(filter.query.urlEncoded())
             }
+
             append("&order=")
             append(
                 when (order) {
@@ -55,6 +60,7 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
                 }
             )
         }
+
         val doc = webClient.httpGet(url).parseHtml()
         return doc.select("section[aria-label='series area'] .card").map { card ->
             val href = card.selectFirstOrThrow("a").attrAsRelativeUrl("href")
@@ -79,8 +85,6 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
         val doc = webClient.httpGet(manga.url.toAbsoluteUrl("uzaymanga.com")).parseHtml()
         val statusText = doc.selectFirst("span:contains(Durum) + span")?.text().orEmpty()
         return manga.copy(
-            altTitles = emptySet(),
-            authors = emptySet(),
             tags = doc.select("a[href^='search?categories']").mapToSet {
                 MangaTag(
                     key = it.text().lowercase(Locale.ROOT),
@@ -89,21 +93,23 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
                 )
             },
             description = doc.selectFirst("div.grid h2 + p")?.text(),
-            state = when {
-                statusText.contains("Devam Ediyor", ignoreCase = true) || statusText.contains("Birakildi", ignoreCase = true) -> MangaState.ONGOING
-                statusText.contains("Tamamlandi", ignoreCase = true) -> MangaState.FINISHED
+            state = when (statusText) {
+                "Devam Ediyor" -> MangaState.ONGOING
+                "Birakildi" -> MangaState.ONGOING
+                "Tamamlandi" -> MangaState.FINISHED
                 else -> null
             },
             chapters = doc.select("div.list-episode a").mapChapters(reversed = true) { i, el ->
                 val href = el.attrAsRelativeUrl("href")
+                val dateFormat = SimpleDateFormat("MMM d ,yyyy", Locale("tr"))
                 MangaChapter(
                     id = generateUid(href),
                     title = el.selectFirstOrThrow("h3").text(),
-                    number = (i + 1).toFloat(),
+                    number = (i + 1).toFloat(), 
                     volume = 0,
                     url = href,
                     scanlator = null,
-                    uploadDate = el.selectFirst("span")?.text()?.let { parseDate(it) } ?: 0L,
+                    uploadDate = el.selectFirst("span")?.text()?.let { dateFormat.tryParse(it) } ?: 0L,
                     branch = null,
                     source = source,
                 )
@@ -113,32 +119,17 @@ internal class UzayManga(context: MangaLoaderContext) : LegacyPagedMangaParser(c
 
     override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
         val doc = webClient.httpGet(chapter.url.toAbsoluteUrl("uzaymanga.com")).parseHtml()
-        val script = doc.select("script").map { it.html() }.firstOrNull { PAGE_REGEX.find(it) != null } ?: return emptyList()
-        return PAGE_REGEX.findAll(script).mapIndexed { idx, result ->
-            val url = result.groups[1]?.value ?: return@mapIndexed null
-            MangaPage(
-                id = generateUid(url),
-                url = "https://cdn1.uzaymanga.com/upload/series/$url",
-                preview = null,
-                source = source,
-            )
-        }.filterNotNull().toList()
-    }
-
-    private suspend fun fetchAvailableTags(): Set<MangaTag> {
-        return emptySet()
-    }
-
-    private fun parseDate(date: String): Long? {
-        return try {
-            DATE_FORMAT.parse(date)?.time
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    companion object {
-        private val DATE_FORMAT = SimpleDateFormat("MMM d ,yyyy", Locale("tr"))
-        private val PAGE_REGEX = Regex("\\\\\"path\\\\\":\\\\\"([^\"]+)\\\\\"")
+        val pageRegex = Regex("\\\\\"path\\\\\":\\\\\"([^\"]+)\\\\\"")
+        val script = doc.select("script").find { it.html().contains(pageRegex) }?.html() ?: return emptyList()
+        return pageRegex.findAll(script).mapNotNull { result ->
+            result.groups[1]?.value?.let { url ->
+                MangaPage(
+                    id = generateUid(url),
+                    url = "https://cdn1.uzaymanga.com/upload/series/$url",
+                    preview = null, 
+                    source = source,
+                )
+            }
+        }.toList()
     }
 }
