9ba16290655cd2a932b161eb39e806d1de264579
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-04-26 19:59:48 +0300

    [AlucardScans] Add source (#1722)

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/AlucardScans.kt
@@ -0,0 +1,16 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.tr
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("ALUCARDSCANS", "AlucardScans", "tr")
+internal class AlucardScans(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.ALUCARDSCANS, "alucardscans.com", 20, 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = true,
+		)
+}
