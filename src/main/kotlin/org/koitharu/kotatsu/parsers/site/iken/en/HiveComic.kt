8d11a4a4193226264902775dcf9445ab3386f6d3
Draken <131387159+dragonx943@users.noreply.github.com>
2025-04-26 21:35:59 +0700

    [HiveComic] Add Broken

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/HiveComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/HiveComic.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.iken.IkenParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken("Need to fix getPages")
 @MangaSourceParser("HIVECOMIC", "HiveComic", "en")
 internal class HiveComic(context: MangaLoaderContext) :
 	IkenParser(context, MangaParserSource.HIVECOMIC, "hivecomic.com") {
