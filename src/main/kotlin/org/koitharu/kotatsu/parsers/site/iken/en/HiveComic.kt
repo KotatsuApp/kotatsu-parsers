cc22783236c27940bb593abb05e6c2539abcb642
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-04-26 14:49:35 +0300

    Create HiveComic.kt

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/HiveComic.kt
@@ -0,0 +1,12 @@
+package org.koitharu.kotatsu.parsers.site.iken.en
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.iken.IkenParser
+
+@MangaSourceParser("HIVECOMIC", "HiveComic", "en")
+internal class HiveComic(context: MangaLoaderContext) :
+	IkenParser(context, MangaParserSource.HIVECOMIC, "hivecomic.com") {
+	override val selectPages = "main section img"
+}
