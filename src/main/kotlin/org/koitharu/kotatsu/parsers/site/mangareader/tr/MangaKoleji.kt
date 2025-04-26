71d79b3b5d3164f087c095a5eeb4e81bca270549
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-04-26 14:46:26 +0300

    Create MangaKoleji.kt

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/MangaKoleji.kt
@@ -0,0 +1,16 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.tr
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("MANGAKOLEJI", "MangaKoleji", "tr")
+internal class MangaKoleji(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.MANGAKOLEJI, "mangakoleji.com", pageSize = 20, searchPageSize = 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = true,
+		)
+}
