5c9c2c3a7a9a8ee866bc50c97ca93308bdae0902
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-04-27 21:46:34 +0300

    [BookManga] Add Source (#1731)

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/BookManga.kt
@@ -0,0 +1,10 @@
+package org.koitharu.kotatsu.parsers.site.madara.en
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+
+@MangaSourceParser("BOOKMANGA", "BookManga", "en")
+internal class BookManga(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.BOOKMANGA, "bookmanga.com", 20)
