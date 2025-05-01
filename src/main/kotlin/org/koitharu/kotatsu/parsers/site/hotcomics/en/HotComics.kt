cee16562393327dc157f027940d3b2ec16a5c4aa
dragonx943 <premieregirl26@gmail.com>
2025-05-01 09:35:51 +0700

    [HotComics] Debug attempt 2

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/hotcomics/en/HotComics.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/hotcomics/en/HotComics.kt
@@ -18,33 +18,28 @@ internal class HotComics(context: MangaLoaderContext) :
 		val mangaUrl = manga.url.toAbsoluteUrl(domain)
 		val redirectHeaders = Headers.Builder().set("Referer", mangaUrl).build()
 		val doc = webClient.httpGet(mangaUrl, redirectHeaders).parseHtml()
-		val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
+		val chapters = doc.select("#tab-chapter a").mapChapters { i, element ->
+			val url = element.attr("onclick").substringAfter("popupLogin('").substringBefore("'")
+			val name = element.selectFirst(".cell-num")?.text() ?: "Unknown"
+			val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
+			val dateUpload = dateFormat.tryParse(element.selectFirst(".cell-time")?.text())
+			val chapterNum = element.selectFirst(".num")?.text()?.toFloat() ?: (i + 1f)
+			MangaChapter(
+				id = generateUid(url),
+				title = name,
+				number = chapterNum,
+				volume = 0,
+				url = url,
+				scanlator = null,
+				uploadDate = dateUpload,
+				branch = null,
+				source = source,
+			)
+		}.reversed()
+
 		return manga.copy(
 			description = doc.selectFirst("div.title_content_box h2")?.text() ?: manga.description,
-			chapters = doc.select(selectMangaChapters).mapChapters { i, li ->
-				val a = li.selectFirstOrThrow("a")
-				val href = a.attr("href")
-				val url = if (href.startsWith("/")) {
-					"/" + href.removePrefix("/").substringAfter('/') // remove /$lang/url
-				} else if (href.startsWith("javascript")) {
-					val h = a.attr("onclick").substringAfterLast("popupLogin('").substringBefore("'")
-					"/" + h.removePrefix("/").substringAfter('/') // remove /$lang/url
-				} else {
-					href
-				}
-				val chapterNum = li.selectFirst(".num")?.text()?.toFloat() ?: (i + 1f)
-				MangaChapter(
-					id = generateUid(url),
-					title = null,
-					number = chapterNum,
-					volume = 0,
-					url = url,
-					scanlator = null,
-					uploadDate = dateFormat.tryParse(li.selectFirst("time")?.attr("datetime")),
-					branch = null,
-					source = source,
-				)
-			},
+			chapters = chapters,
 		)
 	}
 }
