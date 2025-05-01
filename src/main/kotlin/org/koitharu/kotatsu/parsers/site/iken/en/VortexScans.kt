7ad7012993f83ec872835c55228187a227528789
dragonx943 <premieregirl26@gmail.com>
2025-05-01 17:01:03 +0700

    [site/iken] Small fixes

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/VortexScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/VortexScans.kt
@@ -4,9 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.iken.IkenParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken("Need to fix getPages")
 @MangaSourceParser("VORTEXSCANS", "VortexScans", "en")
 internal class VortexScans(context: MangaLoaderContext) :
-	IkenParser(context, MangaParserSource.VORTEXSCANS, "vortexscans.org") {
-	override val selectPages = "main section img"
-}
+	IkenParser(context, MangaParserSource.VORTEXSCANS, "vortexscans.org", 18, true)
