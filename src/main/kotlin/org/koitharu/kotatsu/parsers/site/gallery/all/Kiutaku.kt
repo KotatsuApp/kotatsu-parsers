5b76f6144458893846f6ad2e18108273dabea770
Draken <dragonx943@users.noreply.github.com>
2025-05-04 14:03:14 +0000

    [Kiutaku + Xiutaku] Add sources

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/gallery/all/Kiutaku.kt
@@ -0,0 +1,18 @@
+package org.koitharu.kotatsu.parsers.site.gallery.all
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.config.ConfigKey
+import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.model.MangaListFilterOptions
+import org.koitharu.kotatsu.parsers.site.gallery.GalleryParser
+
+@MangaSourceParser("KIUTAKU", "Kiutaku", type = ContentType.OTHER)
+internal class Kiutaku(context: MangaLoaderContext) :
+    GalleryParser(context, MangaParserSource.KIUTAKU, "kiutaku.com") {
+    
+    override suspend fun getFilterOptions():
+        MangaListFilterOptions = MangaListFilterOptions(availableTags = fetchTags())
+
+}
