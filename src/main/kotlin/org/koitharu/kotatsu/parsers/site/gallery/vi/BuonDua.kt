cc98931147b59decce218c08d91f915f275e28a3
Draken <131387159+dragonx943@users.noreply.github.com>
2025-05-05 13:11:12 +0700

    [Kiutaku + Xiutaku] Add sources (#1755)
    Co-authored-by: Draken <dragonx943@users.noreply.github.com>

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/gallery/vi/BuonDua.kt
@@ -0,0 +1,18 @@
+package org.koitharu.kotatsu.parsers.site.gallery.vi
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.config.ConfigKey
+import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.site.gallery.GalleryParser
+
+@MangaSourceParser("BUONDUA", "Buon Dua", "vi", type = ContentType.OTHER)
+internal class BuonDua(context: MangaLoaderContext) :
+    GalleryParser(context, MangaParserSource.BUONDUA, "buondua.com") {
+
+    override val configKeyDomain: ConfigKey.Domain = ConfigKey.Domain(
+        "buondua.com",
+        "buondua.us",
+    )
+}
