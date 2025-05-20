71ee9eeb95bcb2290adbd772d81ff3ba76fd9916
Naga <94557604+NagaYZ@users.noreply.github.com>
2025-05-20 19:31:45 +0200

    magustoon site structure changed

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/MagusToon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/MagusToon.kt
@@ -3,8 +3,9 @@ package org.koitharu.kotatsu.parsers.site.keyoapp.en
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
-import org.koitharu.kotatsu.parsers.site.keyoapp.KeyoappParser
+import org.koitharu.kotatsu.parsers.site.iken.IkenParser
+
 
 @MangaSourceParser("MAGUSMANGA", "MagusToon", "en")
 internal class MagusToon(context: MangaLoaderContext) :
-	KeyoappParser(context, MangaParserSource.MAGUSMANGA, "magustoon.com")
+	IkenParser(context, MangaParserSource.MAGUSMANGA, "magustoon.org",18,true)
