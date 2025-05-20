34a1dabae4ead3ea98f70b776c4ae64c2e339217
Naga <94557604+NagaYZ@users.noreply.github.com>
2025-05-20 20:15:05 +0200

    [MagusToon] Move to iken (#1794)
    Co-authored-by: Draken <131387159+dragonx943@users.noreply.github.com>

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/MagusToon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/MagusToon.kt
@@ -1,10 +1,10 @@
-package org.koitharu.kotatsu.parsers.site.keyoapp.en
+package org.koitharu.kotatsu.parsers.site.iken.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
-import org.koitharu.kotatsu.parsers.site.keyoapp.KeyoappParser
+import org.koitharu.kotatsu.parsers.site.iken.IkenParser
 
 @MangaSourceParser("MAGUSMANGA", "MagusToon", "en")
 internal class MagusToon(context: MangaLoaderContext) :
-	KeyoappParser(context, MangaParserSource.MAGUSMANGA, "magustoon.com")
+        IkenParser(context, MangaParserSource.MAGUSMANGA, "magustoon.org", 18, true)
