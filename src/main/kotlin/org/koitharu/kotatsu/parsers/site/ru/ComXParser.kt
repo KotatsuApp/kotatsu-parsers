b1e97b2031ff451f28599606d3506787b0558e94
Draken <dragonx943@users.noreply.github.com>
2025-05-26 08:32:22 +0000

    [Com-X] Small changes

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/ru/ComXParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/ru/ComXParser.kt
@@ -85,15 +85,26 @@ internal class ComXParser(context: MangaLoaderContext) :
 		val doc = webClient.httpGet(fullUrl).parseHtml()
 		return doc.select("div.readed.d-flex.short").map { item ->
 			val a = item.selectFirstOrThrow("a.readed__img.img-fit-cover.anim")
-			val titleElement = item.selectFirstOrThrow("h3.readed__title a")
 			val img = item.selectFirst("img[data-src]")
 			val href = a.attrAsRelativeUrl("href")
+			val titleElement = item.selectFirstOrThrow("h3.readed__title a")
+			val (mainTitle, altTitle) = titleElement.text()
+				.split("\\s*/\\s*".toRegex())
+				.map { it.trim() }
+				.let { parts ->
+					when {
+						parts.size >= 2 -> parts[1] to parts[0]
+						parts.isNotEmpty() -> parts[0] to ""
+						else -> "" to ""
+					}
+				}
+
 			Manga(
 				id = generateUid(href),
 				url = href,
 				publicUrl = a.attrAsAbsoluteUrl("href"),
-				title = titleElement.text(),
-				altTitles = emptySet(),
+				title = mainTitle,
+				altTitles = if (altTitle.isNotEmpty()) setOf(altTitle) else emptySet(),
 				authors = emptySet(),
 				description = null,
 				tags = emptySet(),
