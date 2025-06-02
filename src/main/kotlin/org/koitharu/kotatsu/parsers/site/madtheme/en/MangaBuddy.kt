3cb53670287a1d635020401bef194e410acbb82f
Draken <131387159+dragonx943@users.noreply.github.com>
2025-06-03 01:11:42 +0700

    [madtheme/en + wpcomics/vi] Small fixes (#1830)
    * [WpComics] Split NetTruyenX + Small improvements
    
    * [MangaBuddy] Add subdomain
    
    * [MangaPuma] Add subdomain
    
    * [MangaForest] Add subdomain
    
    * [MangaCute] Add subdomain
    
    * [Manhuagui] Change UA to `CHROME_DESKTOP`
    
    * [Mangaxyz] Add subdomain

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madtheme/en/MangaBuddy.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madtheme/en/MangaBuddy.kt
@@ -2,9 +2,36 @@ package org.koitharu.kotatsu.parsers.site.madtheme.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaChapter
+import org.koitharu.kotatsu.parsers.model.MangaPage
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madtheme.MadthemeParser
+import org.koitharu.kotatsu.parsers.util.toAbsoluteUrl
+import org.koitharu.kotatsu.parsers.util.generateUid
+import org.koitharu.kotatsu.parsers.util.parseHtml
 
 @MangaSourceParser("MANGABUDDY", "MangaBuddy", "en")
 internal class MangaBuddy(context: MangaLoaderContext) :
-	MadthemeParser(context, MangaParserSource.MANGABUDDY, "mangabuddy.com")
+	MadthemeParser(context, MangaParserSource.MANGABUDDY, "mangabuddy.com") {
+
+	private val subDomain = "sb.mbcdn.xyz"
+
+	override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
+		val fullUrl = chapter.url.toAbsoluteUrl(domain)
+		val doc = webClient.httpGet(fullUrl).parseHtml()
+		val regexPages = Regex("chapImages\\s*=\\s*['\"](.*?)['\"]")
+		val pages = doc.select("script").firstNotNullOfOrNull { script ->
+			regexPages.find(script.html())?.groupValues?.getOrNull(1)
+		}?.split(',')
+
+		return pages?.map { url ->
+			val cleanUrl = url.substringAfter("/manga")
+			MangaPage(
+				id = generateUid(url),
+				url = "https://$subDomain/manga$cleanUrl",
+				preview = null,
+				source = source,
+			)
+		} ?: emptyList()
+	}
+}
