fa80598e7c81ea923b47a0cdbc9eadb1bac9999f
Draken <131387159+dragonx943@users.noreply.github.com>
2025-06-03 02:58:28 +0700

    [DamCoNuong] Update domain
    Solved a task in #1604

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/DamCoNuong.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/vi/DamCoNuong.kt
@@ -18,7 +18,7 @@ import java.util.*
 internal class DamCoNuong(context: MangaLoaderContext) :
 	LegacyPagedMangaParser(context, MangaParserSource.DAMCONUONG, 30) {
 
-	override val configKeyDomain = ConfigKey.Domain("damconuong.site")
+	override val configKeyDomain = ConfigKey.Domain("damconuong.cam", "damconuong.cc")
 
 	private val availableTags = suspendLazy(initializer = ::fetchTags)
 
