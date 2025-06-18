e398a01f143265c32804a0d9125e72588b11ba0a
Draken <131387159+dragonx943@users.noreply.github.com>
2025-06-19 03:34:26 +0700

    [AgsComics] Fix cover images
    Close #1886

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/AgsComics.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/AgsComics.kt
@@ -1,10 +1,24 @@
 package org.koitharu.kotatsu.parsers.site.keyoapp.en
 
+import org.jsoup.nodes.Element
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.keyoapp.KeyoappParser
+import org.koitharu.kotatsu.parsers.util.styleValueOrNull
+import org.koitharu.kotatsu.parsers.util.cssUrl
 
 @MangaSourceParser("AGSCOMICS", "AgsComics", "en")
 internal class AgsComics(context: MangaLoaderContext) :
-	KeyoappParser(context, MangaParserSource.AGSCOMICS, "agrcomics.com")
+	KeyoappParser(context, MangaParserSource.AGSCOMICS, "agrcomics.com") {
+
+	override val cover: (Element) -> String? = { div ->
+		val coverDiv = div.selectFirst("div.bg-cover[style*=background-image]")
+			?: div.takeIf { it.hasClass("bg-cover") && it.hasAttr("style") }
+			?: div.selectFirst("[style*=background-image]")
+			?: throw Exception("Element or image url not found")
+
+		coverDiv.styleValueOrNull("background-image")?.cssUrl()
+	}
+
+}
