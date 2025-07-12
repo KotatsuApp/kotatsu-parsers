5c76ed3a0948bbc19d48d0414f70037e6604fd76
Baturax <bai@baturax.com>
2025-07-12 19:12:54 +0000

    @Broken jelyring

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Jellyring.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Jellyring.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("JELLYRING", "Jellyring", "tr")
 internal class Jellyring(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.JELLYRING, "jellyring.co")
