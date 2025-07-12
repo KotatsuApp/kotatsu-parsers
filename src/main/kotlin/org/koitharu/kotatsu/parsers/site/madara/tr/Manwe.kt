c030e6f1aa941c32839814da29c266b38976bca3
Baturax <bai@baturax.com>
2025-07-12 19:24:02 +0000

    @Broken manwe

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Manwe.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Manwe.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("MANWE", "Manwe", "tr")
 internal class Manwe(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.MANWE, "manwe.pro", 20)
