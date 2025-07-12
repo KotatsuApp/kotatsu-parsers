631303cdf6121c3c0dd0f100a430bd4fac48dd55
Baturax <bai@baturax.com>
2025-07-12 19:17:00 +0000

    @Broken lichsubs

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/LichSubs.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/LichSubs.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("LICHSUBS", "LichSubs", "tr")
 internal class LichSubs(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.LICHSUBS, "www.kuroimanga.com") {
