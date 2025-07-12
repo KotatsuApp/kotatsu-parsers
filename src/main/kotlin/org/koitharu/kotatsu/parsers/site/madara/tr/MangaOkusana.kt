fa92c11aed076f8ddf95f794e07099059f78a2dd
Baturax <bai@baturax.com>
2025-07-12 19:20:14 +0000

    @Broken mangaokusana

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/MangaOkusana.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/MangaOkusana.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("MANGAOKUSANA", "MangaOkusana", "tr")
 internal class MangaOkusana(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.MANGAOKUSANA, "mangaokusana.com") {
