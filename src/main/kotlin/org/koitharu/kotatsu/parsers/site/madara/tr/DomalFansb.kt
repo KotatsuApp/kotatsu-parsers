96162bf7340a9957e70bf81940704f9d17131ffa
Baturax <bai@baturax.com>
2025-07-12 19:11:56 +0000

    @Broken domalfansub

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/DomalFansb.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/DomalFansb.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken
 @MangaSourceParser("DOMALFANSB", "DomalFansub", "tr")
 internal class DomalFansb(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.DOMALFANSB, "domalfansb.com.tr") {
