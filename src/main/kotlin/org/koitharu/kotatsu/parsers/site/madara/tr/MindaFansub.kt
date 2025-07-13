cc16ee74cc2d941d67bd31988024e097b5808d2f
Baturax <bai@baturax.com>
2025-07-13 04:43:48 +0000

    Broken mindafansub

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/MindaFansub.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/MindaFansub.kt
@@ -1,5 +1,6 @@
 package org.koitharu.kotatsu.parsers.site.madara.tr
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
