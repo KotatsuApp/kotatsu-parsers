69b379da03db4ffe4367a9b73284d774c24012a3
Baturax <bai@baturax.com>
2025-07-13 21:36:13 +0300

    add gafeland

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/tr/Gafeland.kt
@@ -0,0 +1,17 @@
+package org.koitharu.kotatsu.parsers.site.mangareader.tr
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("GAFELAND", "Gafeland", "tr")
+internal class Gafeland(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.GAFELAND, "www.gafeland.com", pageSize = 20, searchPageSize = 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = false,
+		)
+}
+
