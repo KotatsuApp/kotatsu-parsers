601a249aafbc2df5fd39a79769b8e7b59bcdda0f
Naga <94557604+NagaYZ@users.noreply.github.com>
2025-07-28 16:50:55 +0200

    update url

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/fr/RimuScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/fr/RimuScans.kt
@@ -7,4 +7,4 @@ import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
 @MangaSourceParser("RIMUSCANS", "RimuScans", "fr")
 internal class RimuScans(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.RIMUSCANS, "rimuscans.fr", pageSize = 30, searchPageSize = 10)
+	MangaReaderParser(context, MangaParserSource.RIMUSCANS, "rimuscans.com", pageSize = 30, searchPageSize = 10)
