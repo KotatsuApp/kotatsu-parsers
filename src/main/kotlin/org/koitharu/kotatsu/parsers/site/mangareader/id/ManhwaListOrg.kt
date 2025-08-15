0da8e41f27f2aae83d75d1603ba4739f79b9f050
Naga <94557604+NagaYZ@users.noreply.github.com>
2025-08-15 19:17:32 +0200

    update domain

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/ManhwaListOrg.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/ManhwaListOrg.kt
@@ -1,19 +1,23 @@
 package org.koitharu.kotatsu.parsers.site.mangareader.id
 
-import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
-import java.util.*
+import java.util.Locale
 
-@Broken
 @MangaSourceParser("MANHWALIST_ORG", "ManhwaList.org", "id")
 internal class ManhwaListOrg(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.MANHWALIST_ORG, "manhwalist.org", pageSize = 20, searchPageSize = 10) {
+	MangaReaderParser(
+		context,
+		MangaParserSource.MANHWALIST_ORG,
+		"isekaikomik.com",
+		pageSize = 30,
+		searchPageSize = 10,
+	) {
 	override val sourceLocale: Locale = Locale.ENGLISH
-	override val listUrl = "/manhwa"
+	override val listUrl = "/manga"
 	override val filterCapabilities: MangaListFilterCapabilities
 		get() = super.filterCapabilities.copy(
 			isTagsExclusionSupported = false,
