e3b955184e172fc30392b1925855db3483d59be9
Daniil Zhuravlev <nakrovati@outlook.com>
2025-08-25 19:10:05 +0400

    chore: update outdated domains
    #1942, #1952, #2128, #2129

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangabox/en/Mangabat.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangabox/en/Mangabat.kt
@@ -9,7 +9,6 @@ import org.koitharu.kotatsu.parsers.site.mangabox.MangaboxParser
 @MangaSourceParser("HMANGABAT", "MangaBat", "en")
 internal class Mangabat(context: MangaLoaderContext) :
 	MangaboxParser(context, MangaParserSource.HMANGABAT) {
-	override val configKeyDomain = ConfigKey.Domain("h.mangabat.com", "readmangabat.com")
-	override val otherDomain = "readmangabat.com"
+	override val configKeyDomain = ConfigKey.Domain("mangabats.com")
 	override val selectTagMap = "div.panel-category p.pn-category-row:not(.pn-category-row-border) a"
 }
