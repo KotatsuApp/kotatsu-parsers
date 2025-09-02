5962c94c15cc711655348fb559bba7850eb30e1e
Daniil Zhuravlev <nakrovati@outlook.com>
2025-09-02 20:03:01 +0400

    chore: update outdated domains

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/SirenKomik.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/SirenKomik.kt
@@ -8,7 +8,7 @@ import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
 @MangaSourceParser("SIRENKOMIK", "SirenKomik", "id")
 internal class SirenKomik(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.SIRENKOMIK, "sirenkomik.my.id", pageSize = 20, searchPageSize = 10) {
+	MangaReaderParser(context, MangaParserSource.SIRENKOMIK, "sirenkomik.xyz", pageSize = 20, searchPageSize = 10) {
 	override val filterCapabilities: MangaListFilterCapabilities
 		get() = super.filterCapabilities.copy(
 			isTagsExclusionSupported = false,
