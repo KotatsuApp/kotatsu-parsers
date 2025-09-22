d8e47b2b525c84b5100507eb8485bb9f7d1ab38a
dragonx943 <premieregirl26@gmail.com>
2025-09-22 11:21:13 +0700

    HiveComic: Remove broken

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/HiveComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/HiveComic.kt
@@ -4,9 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.iken.IkenParser
-import org.koitharu.kotatsu.parsers.Broken
 
-@Broken("Need to fix getPages")
 @MangaSourceParser("HIVECOMIC", "HiveComic", "en")
 internal class HiveComic(context: MangaLoaderContext) :
 	IkenParser(context, MangaParserSource.HIVECOMIC, "hivetoons.org", 18, true)
