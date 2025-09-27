e58f9b0c78135591eee77f50042ab2721fc31049
dragonx943 <premieregirl26@gmail.com>
2025-09-27 18:55:13 +0700

    MadaraDex: Fix User-Agent key + Remove from source config

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/MadaraDex.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/MadaraDex.kt
@@ -5,17 +5,26 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.config.ConfigKey
 import org.koitharu.kotatsu.parsers.exception.ParseException
 import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaChapter
 import org.koitharu.kotatsu.parsers.model.MangaPage
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.network.UserAgents
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 import org.koitharu.kotatsu.parsers.util.*
 
 @MangaSourceParser("MADARADEX", "MadaraDex", "en", ContentType.HENTAI)
 internal class MadaraDex(context: MangaLoaderContext) :
     MadaraParser(context, MangaParserSource.MADARADEX, "madaradex.org") {
+
+    override val userAgentKey = ConfigKey.UserAgent(UserAgents.CHROME_DESKTOP)
+
+    override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
+        super.onCreateConfig(keys)
+        keys.remove(userAgentKey)
+    }
 
     override fun getRequestHeaders() = super.getRequestHeaders().newBuilder()
         .add("sec-fetch-site", "same-site")
@@ -55,6 +64,7 @@ internal class MadaraDex(context: MangaLoaderContext) :
             val newReq = request.newBuilder()
                 .header("sec-fetch-site", "same-site")
                 .header("Referer", fullUrl)
+                .header("User-Agent", userAgentKey.toString())
                 .url(cleanUrl)
                 .build()
             chain.proceed(newReq)
