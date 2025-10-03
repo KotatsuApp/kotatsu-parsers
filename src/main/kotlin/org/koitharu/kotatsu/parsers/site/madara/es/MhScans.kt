6f761a65a6b762e69b1f0804d0eebaf2fe3ed7db
Vicente <vicente@per>
2025-10-03 08:58:03 +0200

    Update mhscans

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/MhScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/MhScans.kt
@@ -7,7 +7,7 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("MHSCANS", "MhScans", "es")
 internal class MhScans(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.MHSCANS, "twobluescans.com") {
+	MadaraParser(context, MangaParserSource.MHSCANS, "mhscans.mundoalterno.org") {
 	override val datePattern = "d 'de' MMMMM 'de' yyyy"
 	override val listUrl = "series/"
 }
