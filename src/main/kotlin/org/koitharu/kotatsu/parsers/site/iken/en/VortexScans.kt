533b884f351d5afb0c02f9b56fa30e39e077bfd0
dragonx943 <premieregirl26@gmail.com>
2025-10-26 20:09:32 +0700

    VortexScans: Remove broken annotation

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/VortexScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/iken/en/VortexScans.kt
@@ -4,9 +4,7 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.iken.IkenParser
-import org.koitharu.kotatsu.parsers.Broken
 
-@Broken("Need to fix getPages")
 @MangaSourceParser("VORTEXSCANS", "VortexScans", "en")
 internal class VortexScans(context: MangaLoaderContext) :
 	IkenParser(context, MangaParserSource.VORTEXSCANS, "vortexscans.org", 18, true)
